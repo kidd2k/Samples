8bf379745427f6e928b15e06f6110eb62c614010
Kirby L. Cooper <kidd2kx@gmail.com>
2021-09-01 11:49:42 -0500

    + 'tabs to spaces' editing for GitHub

--- VulkanRenderer/VKNBatchDrawEffect.h
+++ VulkanRenderer/VKNBatchDrawEffect.h
@@ -15,111 +15,111 @@
 
 namespace GamePrototype
 {
-	class IVKNMultiDraw;
-	typedef std::shared_ptr<IVKNMultiDraw> VKNMultiDrawPtr;
+    class IVKNMultiDraw;
+    typedef std::shared_ptr<IVKNMultiDraw> VKNMultiDrawPtr;
 
-	class VKNBatchDrawEffect : public BatchDrawEffect
-	{
-	public:
-		explicit VKNBatchDrawEffect(const EffectInitInfo&);
+    class VKNBatchDrawEffect : public BatchDrawEffect
+    {
+    public:
+        explicit VKNBatchDrawEffect(const EffectInitInfo&);
 
-	protected:
+    protected:
 
-		// IEffect
-		virtual ~VKNBatchDrawEffect() override;
-		virtual bool Init() override;
-		virtual int NumPasses() const override;
-		virtual bool PrePass(int, const EffectStatePtr&) override;
-		virtual bool Collect(const Graphics::RenderObjectPtr&) override;
-		virtual bool Collect(const std::vector<Graphics::RenderObjectPtr>&) override;
-		virtual bool Collect(const Graphics::SceneNodePtr&) override;
-		virtual bool Draw(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&, const EffectStatePtr&) override;
-		virtual bool PostPass(const EffectStatePtr&) override;
-		virtual void ClearForNextFrame() override;
-		virtual int GetID() const override;
-		virtual void Free() override;
-		virtual int GetEffectType() const override;
-		virtual bool PostSceneGraph() override;
+        // IEffect
+        virtual ~VKNBatchDrawEffect() override;
+        virtual bool Init() override;
+        virtual int NumPasses() const override;
+        virtual bool PrePass(int, const EffectStatePtr&) override;
+        virtual bool Collect(const Graphics::RenderObjectPtr&) override;
+        virtual bool Collect(const std::vector<Graphics::RenderObjectPtr>&) override;
+        virtual bool Collect(const Graphics::SceneNodePtr&) override;
+        virtual bool Draw(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&, const EffectStatePtr&) override;
+        virtual bool PostPass(const EffectStatePtr&) override;
+        virtual void ClearForNextFrame() override;
+        virtual int GetID() const override;
+        virtual void Free() override;
+        virtual int GetEffectType() const override;
+        virtual bool PostSceneGraph() override;
 
-	private:
+    private:
 
-		bool DrawStaticPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
-		bool DrawDynamicPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
+        bool DrawStaticPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
+        bool DrawDynamicPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
 
-		bool CheckBuffers();
+        bool CheckBuffers();
 
-		bool UpdateUniforms(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
+        bool UpdateUniforms(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
 
-		bool CreateMemBufferHelpers();
+        bool CreateMemBufferHelpers();
 
-		bool SetupStaticShadowShader(const VKNShaderPtr&, const VKNEffectState&);
-		bool SetupStaticShader(const VKNShaderPtr&, const VKNEffectState&);
-		bool SetupDynamicShadowShader(const VKNShaderPtr&, const VKNEffectState&);
-		bool SetupDynamicShader(const VKNShaderPtr&, const VKNEffectState&);
-		bool SetupStaticAlphaBlendShader(const VKNShaderPtr&, const VKNEffectState&);
-		bool SetupDynamicAlphaBlendShader(const VKNShaderPtr&, const VKNEffectState&);
+        bool SetupStaticShadowShader(const VKNShaderPtr&, const VKNEffectState&);
+        bool SetupStaticShader(const VKNShaderPtr&, const VKNEffectState&);
+        bool SetupDynamicShadowShader(const VKNShaderPtr&, const VKNEffectState&);
+        bool SetupDynamicShader(const VKNShaderPtr&, const VKNEffectState&);
+        bool SetupStaticAlphaBlendShader(const VKNShaderPtr&, const VKNEffectState&);
+        bool SetupDynamicAlphaBlendShader(const VKNShaderPtr&, const VKNEffectState&);
 
-		// sanity testing
-		bool FakePrePass(int, const EffectStatePtr&);
-		bool FakeDraw(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
-		bool FakePostPass();
-		bool FakeSetupShader(const VKNShaderPtr&, const VKNEffectState&);
-		int FakeNumPasses() const { return 1; }
+        // sanity testing
+        bool FakePrePass(int, const EffectStatePtr&);
+        bool FakeDraw(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
+        bool FakePostPass();
+        bool FakeSetupShader(const VKNShaderPtr&, const VKNEffectState&);
+        int FakeNumPasses() const { return 1; }
 
-		VKNMultiDrawPtr							m_dynamicMultiDrawObjectPtr;
-		VKNMultiDrawPtr							m_staticMultiDrawObjectPtr;
-		VKNMultiDrawPtr							m_alphaDynamicMultiDrawObjectPtr;
-		VKNMultiDrawPtr							m_alphaStaticMultiDrawObjectPtr;
-		int										m_id;
-		int										m_currentPass;
-		const int								m_effectType;			// IEffectMgr::EffectType enum
-		std::vector<ShaderPtr>					m_cachedShaderPtrs;
-		bool									m_bIsFirstDynamic;
-		bool									m_bIsFirstStatic;
-		bool									m_bIsFirstAlphaDynamic;
-		bool									m_bIsFirstAlphaStatic;
-		bool									m_bIsInitialized;
-		TexturePackPtr							m_texPackPtr;
-		std::vector<DrawPackageDataPtr>			m_staticPackages;
-		std::vector<DrawPackageDataPtr>			m_dynamicPackages;
-		bool									m_bSetStaticPackages;
-		bool									m_bSetDynamicPackages;
+        VKNMultiDrawPtr                            m_dynamicMultiDrawObjectPtr;
+        VKNMultiDrawPtr                            m_staticMultiDrawObjectPtr;
+        VKNMultiDrawPtr                            m_alphaDynamicMultiDrawObjectPtr;
+        VKNMultiDrawPtr                            m_alphaStaticMultiDrawObjectPtr;
+        int                                        m_id;
+        int                                        m_currentPass;
+        const int                                  m_effectType;            // IEffectMgr::EffectType enum
+        std::vector<ShaderPtr>                     m_cachedShaderPtrs;
+        bool                                       m_bIsFirstDynamic;
+        bool                                       m_bIsFirstStatic;
+        bool                                       m_bIsFirstAlphaDynamic;
+        bool                                       m_bIsFirstAlphaStatic;
+        bool                                       m_bIsInitialized;
+        TexturePackPtr                             m_texPackPtr;
+        std::vector<DrawPackageDataPtr>            m_staticPackages;
+        std::vector<DrawPackageDataPtr>            m_dynamicPackages;
+        bool                                       m_bSetStaticPackages;
+        bool                                       m_bSetDynamicPackages;
 
-		// this type does need its own pipeline to bind to shaders used locally
-		VKNPipelineBuilder						m_pipelineBuilder;
+        // this type does need its own pipeline to bind to shaders used locally
+        VKNPipelineBuilder                        m_pipelineBuilder;
 
-		// DescriptorSet (texturePack & uniform buffer for shader vars)
-		// binding = 0
-		struct UniformData
-		{
-			/* 
-				TODO: whatever uniform data each shader needs can be added to this struct,
-				and copied to mapped buffer memory and bound to the command buffer.
-				Just make sure your alignments (OpenGL std 140?) are correct.
-			*/
-			float viewCam[Math::mat4::MAT4_SIZE];
-			float projCam[Math::mat4::MAT4_SIZE];
+        // DescriptorSet (texturePack & uniform buffer for shader vars)
+        // binding = 0
+        struct UniformData
+        {
+            /* 
+                TODO: whatever uniform data each shader needs can be added to this struct,
+                and copied to mapped buffer memory and bound to the command buffer.
+                Just make sure your alignments (OpenGL std 140?) are correct.
+            */
+            float viewCam[Math::mat4::MAT4_SIZE];
+            float projCam[Math::mat4::MAT4_SIZE];
 
-			UniformData()
-			:
-			viewCam{},
-			projCam{}
-			{
-				memcpy(viewCam, Math::mat4::Identity().Get(), Math::mat4::MAT4_SIZE*sizeof(float));
-				memcpy(projCam, Math::mat4::Identity().Get(), Math::mat4::MAT4_SIZE*sizeof(float));
-			}
+            UniformData()
+            :
+            viewCam{},
+            projCam{}
+            {
+                memcpy(viewCam, Math::mat4::Identity().Get(), Math::mat4::MAT4_SIZE*sizeof(float));
+                memcpy(projCam, Math::mat4::Identity().Get(), Math::mat4::MAT4_SIZE*sizeof(float));
+            }
 
-			static bool Register(BufferMemoryHelper<UniformData>& bmh)
-			{
-				CheckOK(bmh.RegisterMember(IBufferMemoryHelper::kMemberType_Mat4, "viewCam", offsetof(UniformData, viewCam)));
-				CheckOK(bmh.RegisterMember(IBufferMemoryHelper::kMemberType_Mat4, "projCam", offsetof(UniformData, projCam)));
-				return true;
-			}
-		};
+            static bool Register(BufferMemoryHelper<UniformData>& bmh)
+            {
+                CheckOK(bmh.RegisterMember(IBufferMemoryHelper::kMemberType_Mat4, "viewCam", offsetof(UniformData, viewCam)));
+                CheckOK(bmh.RegisterMember(IBufferMemoryHelper::kMemberType_Mat4, "projCam", offsetof(UniformData, projCam)));
+                return true;
+            }
+        };
 
-		BufferMemoryHelperPtr m_uniformMemHelperPtr;
+        BufferMemoryHelperPtr m_uniformMemHelperPtr;
 
-		static const int VERTEX_BUFFER_BIND_ID = 0;
-	};
+        static const int VERTEX_BUFFER_BIND_ID = 0;
+    };
 }
 #endif // VKN_BATCH_DRAW_EFFECT_H
