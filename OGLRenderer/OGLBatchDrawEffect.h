3bf164dbee307a5fd50873285aaff727a1792b8a
Kirby L. Cooper <kidd2kx@gmail.com>
2021-09-01 11:42:03 -0500

    + editing 'tabs to spaces' formatting for GitHub again...

--- OGLRenderer/OGLBatchDrawEffect.h
+++ OGLRenderer/OGLBatchDrawEffect.h
@@ -10,55 +10,55 @@
 
 namespace GamePrototype
 {
-	class OGLBatchDrawEffect : public BatchDrawEffect
-	{
-	public:
-		explicit OGLBatchDrawEffect(const EffectInitInfo&);
+    class OGLBatchDrawEffect : public BatchDrawEffect
+    {
+    public:
+        explicit OGLBatchDrawEffect(const EffectInitInfo&);
 
-	protected:
+    protected:
 
-		// IEffect
-		virtual bool Init() override;
-		virtual int NumPasses() const override;
-		virtual bool PrePass(int, const EffectStatePtr&) override;
-		virtual bool Collect(const Graphics::RenderObjectPtr&) override;
-		virtual bool Collect(const std::vector<Graphics::RenderObjectPtr>&) override;
-		virtual bool Collect(const Graphics::SceneNodePtr&) override;
-		virtual bool Draw(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&, const EffectStatePtr&) override;
-		virtual bool PostPass(const EffectStatePtr&) override;
-		virtual void ClearForNextFrame() override;
-		virtual int GetID() const override;
-		virtual void Free() override;
-		virtual int GetEffectType() const override;
-		virtual bool PostSceneGraph() override;
+        // IEffect
+        virtual bool Init() override;
+        virtual int NumPasses() const override;
+        virtual bool PrePass(int, const EffectStatePtr&) override;
+        virtual bool Collect(const Graphics::RenderObjectPtr&) override;
+        virtual bool Collect(const std::vector<Graphics::RenderObjectPtr>&) override;
+        virtual bool Collect(const Graphics::SceneNodePtr&) override;
+        virtual bool Draw(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&, const EffectStatePtr&) override;
+        virtual bool PostPass(const EffectStatePtr&) override;
+        virtual void ClearForNextFrame() override;
+        virtual int GetID() const override;
+        virtual void Free() override;
+        virtual int GetEffectType() const override;
+        virtual bool PostSceneGraph() override;
 
-	private:
+    private:
 
-		bool DrawStaticPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
-		bool DrawDynamicPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
+        bool DrawStaticPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
+        bool DrawDynamicPass(const Graphics::CameraDrawInfo&, const Graphics::RenderStateInfo&);
 
-		bool CheckBuffers();
+        bool CheckBuffers();
 
-		MultiDrawPtr							m_dynamicMultiDrawObjectPtr;
-		MultiDrawPtr							m_staticMultiDrawObjectPtr;
-		MultiDrawPtr							m_alphaDynamicMultiDrawObjectPtr;
-		MultiDrawPtr							m_alphaStaticMultiDrawObjectPtr;
-		int										m_id;
-		int										m_currentPass;
-		const int								m_effectType;			// IEffectMgr::EffectType enum
-		std::vector<ShaderPtr>					m_cachedShaderPtrs;
-		bool									m_bIsFirstDynamic;
-		bool									m_bIsFirstStatic;
-		bool									m_bIsFirstAlphaDynamic;
-		bool									m_bIsFirstAlphaStatic;
-		bool									m_bIsInitialized;
-		TexturePackPtr							m_texPackPtr;
-		TexturePackPtr							m_alphaTexPackPtr;
-		std::vector<DrawPackageDataPtr>			m_staticPackages;
-		std::vector<DrawPackageDataPtr>			m_dynamicPackages;
-		bool									m_bSetStaticPackages;
-		bool									m_bSetDynamicPackages;
-	};
+        MultiDrawPtr                            m_dynamicMultiDrawObjectPtr;
+        MultiDrawPtr                            m_staticMultiDrawObjectPtr;
+        MultiDrawPtr                            m_alphaDynamicMultiDrawObjectPtr;
+        MultiDrawPtr                            m_alphaStaticMultiDrawObjectPtr;
+        int                                     m_id;
+        int                                     m_currentPass;
+        const int                               m_effectType;            // IEffectMgr::EffectType enum
+        std::vector<ShaderPtr>                  m_cachedShaderPtrs;
+        bool                                    m_bIsFirstDynamic;
+        bool                                    m_bIsFirstStatic;
+        bool                                    m_bIsFirstAlphaDynamic;
+        bool                                    m_bIsFirstAlphaStatic;
+        bool                                    m_bIsInitialized;
+        TexturePackPtr                          m_texPackPtr;
+        TexturePackPtr                          m_alphaTexPackPtr;
+        std::vector<DrawPackageDataPtr>         m_staticPackages;
+        std::vector<DrawPackageDataPtr>         m_dynamicPackages;
+        bool                                    m_bSetStaticPackages;
+        bool                                    m_bSetDynamicPackages;
+    };
 }
 
 #endif // OGL_BATCH_DRAW_EFFECT_H
